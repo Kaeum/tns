5a55dd22ac5ace83167880a3d53c66258504c8d1
Kaeum <kamy.na@kakaocorp.com>
2022-09-16 12:09:38 +0900

    changed some details

--- base.py
+++ base.py
@@ -8,14 +8,15 @@ from webdriver_manager.firefox import GeckoDriverManager
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver import FirefoxOptions
 
 # https://nid.naver.com/nidlogin.login?url=https%3A%2F%2Fbooking.naver.com%2Fbooking%2F12%2Fbizes%2F714379%2Fitems%2F4559249
 # https://nid.naver.com/nidlogin.login?url=https%3A%2F%2Fbooking.naver.com%2Fbooking%2F12%2Fbizes%2F714379%2Fitems%2F4559345
 # https://nid.naver.com/nidlogin.login?url=https%3A%2F%2Fbooking.naver.com%2Fbooking%2F12%2Fbizes%2F714379%2Fitems%2F4559346
 # https://nid.naver.com/nidlogin.login?url=https%3A%2F%2Fbooking.naver.com%2Fbooking%2F12%2Fbizes%2F714379%2Fitems%2F4559349
-COURT_URL = "https://nid.naver.com/nidlogin.login?url=https%3A%2F%2Fbooking.naver.com%2Fbooking%2F12%2Fbizes%2F714379%2Fitems%2F4559249"
+COURT_URL = "https://nid.naver.com/nidlogin.login?url=https%3A%2F%2Fbooking.naver.com%2Fbooking%2F12%2Fbizes%2F714379%2Fitems%2F4607947"
 TIME = '14:00'
-DATE = '"2022-09-28"'
+DATE = '"2022-09-29"'
 BANK_NAME = '신한'
 
 user = {
@@ -45,11 +46,11 @@ driver.find_element(By.XPATH, '//*[@id="log.login"]').click()
 
 ##일정선택(날짜,시간)
 
-#날짜
-wait.until(ec.element_to_be_clickable((By.XPATH, ('//*[@data-tst_cal_datetext=%s]' % DATE)))).click()
-
+#날짜 - 여기에 예외처리 추가해야하나..?
+element = driver.find_element(By.XPATH, ('//td[@data-tst_cal_datetext=%s]' % DATE))
+element.click()
 #시간
-wait.until(ec.element_to_be_clickable((By.XPATH, ('//*[text()[contains(., "%s")]]' % TIME)))).click()
+wait.until(ec.element_to_be_clickable((By.XPATH, ('//span[contains(., "%s")]' % TIME)))).click()
 
 wait.until(ec.element_to_be_clickable((By.XPATH, '//button[@data-tst_next_step_click="0"]'))).click()
 
@@ -74,7 +75,7 @@ driver.find_element(By.XPATH, '//button[@data-tst_submit_click="0"]').click()
 ##주문/결제
 
 #일반결제 클릭
-wait.until(ec.element_to_be_clickable((By.XPATH, '//li[contains(@class, "_generalPaymentsTab")]/div/span'))).click()
+wait.until(ec.element_to_be_clickable((By.XPATH, '//li[contains(@class, "_generalPaymentsTab")]/div/span[following-sibling::span]'))).click()
 
 #나중에 결제
 ActionChains(driver).send_keys(Keys.TAB)\
--- window.py
+++ window.py
@@ -15,7 +15,7 @@ from selenium.webdriver.support import expected_conditions as ec
 # https://nid.naver.com/nidlogin.login?url=https%3A%2F%2Fbooking.naver.com%2Fbooking%2F12%2Fbizes%2F714379%2Fitems%2F4559349
 COURT_URL = "https://nid.naver.com/nidlogin.login?url=https%3A%2F%2Fbooking.naver.com%2Fbooking%2F12%2Fbizes%2F714379%2Fitems%2F4559249"
 TIME = '14:00'
-DATE = '"2022-09-28"'
+DATE = '"2022-09-29"'
 BANK_NAME = '신한'
 
 user = {
@@ -29,7 +29,7 @@ def clipboard_input(user_xpath, user_input):
 
     pyperclip.copy(user_input)
     driver.find_element(By.XPATH, user_xpath).click()
-    ActionChains(driver).key_down(Keys.COMMAND).send_keys('v').key_up(Keys.COMMAND).perform()
+    ActionChains(driver).key_down(Keys.CONTROL).send_keys('v').key_up(Keys.CONTROL).perform()
 
     pyperclip.copy(temp_user_input)
 
@@ -49,7 +49,7 @@ driver.find_element(By.XPATH, '//*[@id="log.login"]').click()
 wait.until(ec.element_to_be_clickable((By.XPATH, ('//*[@data-tst_cal_datetext=%s]' % DATE)))).click()
 
 #시간
-wait.until(ec.element_to_be_clickable((By.XPATH, ('//*[text()[contains(., "%s")]]' % TIME)))).click()
+wait.until(ec.element_to_be_clickable((By.XPATH, ('//span[contains(., "%s")]' % TIME)))).click()
 
 wait.until(ec.element_to_be_clickable((By.XPATH, '//button[@data-tst_next_step_click="0"]'))).click()
 
@@ -63,9 +63,9 @@ clipboard_input('//input[@id="extra0"]', user.get("address"))
 
 #셀렉박스(동의합니다)
 wait.until(ec.element_to_be_clickable((By.XPATH, '//div[@class="wrap_select"]'))).click()
-ActionChains(driver).send_keys(Keys.TAB)\
-    .send_keys(Keys.TAB)\
-    .send_keys(Keys.ENTER)\
+ActionChains(driver).send_keys(Keys.TAB) \
+    .send_keys(Keys.TAB) \
+    .send_keys(Keys.ENTER) \
     .perform()
 
 #결제하기 버튼
@@ -74,16 +74,17 @@ driver.find_element(By.XPATH, '//button[@data-tst_submit_click="0"]').click()
 ##주문/결제
 
 #일반결제 클릭
-wait.until(ec.element_to_be_clickable((By.XPATH, '//li[contains(@class, "_generalPaymentsTab")]/div/span'))).click()
+wait.until(ec.element_to_be_clickable((By.XPATH, '//li[contains(@class, "_generalPaymentsTab")]/div/span[following-sibling::span]'))).click()
 
 #나중에 결제
-ActionChains(driver).send_keys(Keys.TAB)\
-    .send_keys(Keys.TAB)\
-    .send_keys(Keys.ARROW_DOWN)\
+ActionChains(driver).send_keys(Keys.TAB) \
+    .send_keys(Keys.TAB) \
+    .send_keys(Keys.ARROW_DOWN) \
     .perform()
 
 #환불 방법 - 환불정산액 적립
 ActionChains(driver).send_keys(Keys.TAB) \
+    .send_keys(Keys.TAB) \
     .send_keys(Keys.TAB) \
     .send_keys(Keys.ARROW_DOWN) \
     .perform()
